28cd3a8e322d811ea220f8f8069df7ec4be7d73b
Charonum <78572976+Charonum@users.noreply.github.com>
2021-04-26 16:09:47 -0700

    Add files via upload

--- Installer.py
+++ Installer.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import getpass
 import shutil
+import zipfile
 
 JS_logo = fr'C:\Users\{getpass.getuser()}\Downloads\JS_Setup\icons\JS logo.ico'
 phone_logo = fr'C:\Users\{getpass.getuser()}\Downloads\JS_Setup\icons\phone.ico'
@@ -27,21 +28,18 @@ subprocess.call([sys.executable, "-m", "pip", "install", "wget"])
 import requests
 import wget
 
-response = requests.get('https://raw.githubusercontent.com/Charonum/JSCode/main/Files.txt')
-responsecontent = response.text
+url = "https://github.com/Charonum/JS_Startup/archive/refs/heads/main.zip"
+wget.download(url, rf"C:\Users\{getpass.getuser()}\Charonum\JS")
 
-for file in responsecontent.split("\n"):
-    file = file.replace("b'", "")
-    file = file.replace("'", "")
-    file = file.replace(r"\n", "")
-    if file == "":
-        pass
-    else:
-        url = f'https://raw.githubusercontent.com/Charonum/JSCode/main/code/windows/{file}'
-        print(f"Fetching {url}")
-        wget.download(url, rf"C:\Users\{getpass.getuser()}\Charonum\JS")
+with zipfile.ZipFile(rf"C:\Users\{getpass.getuser()}\Charonum\JS\JS_Startup-main.zip", 'r') as zipObj:
+    zipObj.extractall()
 
-import getpass
+for file in os.listdir(rf"C:\Users\{getpass.getuser()}\Charonum\JS\JS_Startup-main"):
+    shutil.move(rf"C:\Users\{getpass.getuser()}\Charonum\JS\JS_Startup-main\{file}", rf"C:\Users\{getpass.getuser()}\Charonum\JS")
+
+os.remove(rf"C:\Users\{getpass.getuser()}\Charonum\JS\JS_Startup-main.zip")
+os.remove(rf"C:\Users\{getpass.getuser()}\Charonum\JS\JS_Startup-main")
+
 print("Installing PyAudio...")
 user = getpass.getuser()
 os.chdir(fr"C:\Users\{user}\Downloads\JS_Setup\Setup_Files")
